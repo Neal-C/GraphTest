779388bf958689f604e27ba2d7cf53d66cb1471c
Neal-C <pop6d531@duck.com>
2023-10-05 20:38:07 +0200

    feat(writing all the tests)

--- graph.spec.ts
+++ graph.spec.ts
@@ -2,7 +2,6 @@ import { it, expect, describe } from "vitest";
 import { Graph } from "./graph";
 
 describe("graph", () => {
-	it("should be a blank test for now", () => {});
 	describe("basic initialization", () => {
 		it("should pass basic initiliazing", () => {
 			const graph = new Graph(0, [[]]);
@@ -13,6 +12,53 @@ describe("graph", () => {
 			const graph = new Graph(1, [[2]]);
 			graph.setMaxRun();
 			expect(graph.maxRun).toEqual(2);
+		});
+	});
+
+	describe("more cases", () => {
+		it.each([
+			{
+				size: 2,
+				cases: [
+					[2, 5],
+					[4, 2],
+				],
+				expected: 9,
+			},
+			{
+				size: 3,
+				cases: [
+					[4, 10, 0],
+					[9, 0, 1],
+					[5, 0, 3],
+				],
+				expected: 21,
+			},
+			{
+				size: 4,
+				cases: [
+					[4, 3, 4, 5],
+					[2, 1, 1, 2],
+					[5, 3, 1, 3],
+					[1, 1, 2, 1],
+				],
+				expected: 22,
+			},
+			{
+				size: 5,
+				cases: [],
+				expected: 28,
+			},
+			{
+				size: 6,
+				cases: [],
+				expected: 48,
+			},
+		])("new Graph($size, $cases) -> $expected", ({ size, cases, expected }) => {
+			const graph = new Graph(size, cases);
+			graph.computeMaxRun();
+			const maxRun = graph.getMaxRun();
+			expect(maxRun).toEqual(expected);
 		});
 	});
 });
